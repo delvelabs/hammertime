e084dd1c951a7ddbf6758383b016ceac5d2c4099
Nicolas <nicolas.aubry@delvelabs.ca>
2017-12-18 14:41:13 -0500

    Tests refactoring

--- tests/rule_detect_behavior_change_test.py
+++ tests/rule_detect_behavior_change_test.py
@@ -28,116 +28,103 @@ from hammertime.rules.simhash import Simhash
 
 class TestDetectBehaviorChange(TestCase):
 
+    def setUp(self):
+        self.entry = Entry.create("http://example.com/", response=StaticResponse(200, {}, content="data"))
+        self.behavior_detection = DetectBehaviorChange()
+        self.kb = KnowledgeBase()
+        self.behavior_detection.set_kb(self.kb)
+
     @async_test()
     async def test_after_response_store_simhash_of_response_content_in_knowledge_base(self):
-        entry = Entry.create("http://example.com/", response=StaticResponse(200, {}, content="data"))
-        behavior_detection = DetectBehaviorChange()
-        kb = KnowledgeBase()
-        behavior_detection.set_kb(kb)
+        await self.behavior_detection.after_response(self.entry)
 
-        await behavior_detection.after_response(entry)
-
-        self.assertEqual(kb.behavior_buffer, [Simhash(entry.response.content).value])
+        self.assertEqual(self.kb.behavior_buffer, [Simhash(self.entry.response.content).value])
 
     @async_test()
     async def test_after_response_pop_first_result_when_buffer_is_full(self):
-        entry = Entry.create("http://example.com/", response=StaticResponse(200, {}, content="data"))
-        behavior_detection = DetectBehaviorChange(buffer_size=5)
-        behavior_detection.response_simhash_buffer = ["1", "2", "3", "4", "5"]
+        self.kb.behavior_buffer.extend([str(i) for i in range(10)])
 
-        await behavior_detection.after_response(entry)
+        await self.behavior_detection.after_response(self.entry)
 
-        self.assertEqual(behavior_detection.response_simhash_buffer, ["2", "3", "4", "5", Simhash("data").value])
+        self.assertEqual(self.kb.behavior_buffer, ["1", "2", "3", "4", "5", "6", "7", "8", "9", Simhash("data").value])
 
     @async_test()
     async def test_after_response_test_behavior_if_behavior_buffer_is_full(self):
-        behavior_detection = DetectBehaviorChange(buffer_size=10)
-        behavior_detection.response_simhash_buffer = ["data"] * 10
-        behavior_detection._is_error_behavior = MagicMock(return_value=False)
-        response = StaticResponse(200, {}, content="test")
-        entry = Entry.create("http://example.com/", response=response)
+        self.kb.behavior_buffer.extend(["data"] * 10)
+        self.behavior_detection._is_error_behavior = MagicMock(return_value=False)
 
-        await behavior_detection.after_response(entry)
+        await self.behavior_detection.after_response(self.entry)
 
-        behavior_detection._is_error_behavior.assert_called_once_with(entry)
+        self.behavior_detection._is_error_behavior.assert_called_once_with(self.entry)
 
     @async_test()
     async def test_after_response_dont_test_behavior_if_behavior_buffer_not_full(self):
-        behavior_detection = DetectBehaviorChange(buffer_size=10)
-        behavior_detection.response_simhash_buffer = ["data"] * 5
-        behavior_detection._is_error_behavior = MagicMock(return_value=False)
-        response = StaticResponse(200, {}, content="test")
-        entry = Entry.create("http://example.com/", response=response)
+        self.kb.behavior_buffer.extend(["data"] * 5)
+        self.behavior_detection._is_error_behavior = MagicMock(return_value=False)
 
-        await behavior_detection.after_response(entry)
+        await self.behavior_detection.after_response(self.entry)
 
-        behavior_detection._is_error_behavior.assert_not_called()
+        self.behavior_detection._is_error_behavior.assert_not_called()
 
     @async_test()
     async def test_after_response_set_flag_in_entry_arguments_if_behavior_is_not_normal(self):
-        behavior_detection = DetectBehaviorChange(buffer_size=10)
-        behavior_detection.response_simhash_buffer = ["data"] * 10
-        response = StaticResponse(200, {}, content="data")
-        entry = Entry.create("http://example.com/", response=response)
+        self.kb.behavior_buffer.extend(["data"] * 10)
 
-        await behavior_detection.after_response(entry)
+        await self.behavior_detection.after_response(self.entry)
 
-        self.assertTrue(entry.arguments["error_behavior"])
-        self.assertTrue(behavior_detection.error_behavior)
+        self.assertTrue(self.entry.arguments["error_behavior"])
+        self.assertTrue(self.behavior_detection.error_behavior)
 
     @async_test()
     async def test_after_response_dont_set_flag_in_entry_arguments_if_normal_behavior_restored(self):
-        behavior_detection = DetectBehaviorChange(buffer_size=10)
-        behavior_detection.response_simhash_buffer = ["data"] * 10
-        behavior_detection.error_behavior = True
-        response = StaticResponse(200, {}, content="test")
-        entry = Entry.create("http://example.com/", response=response)
+        self.kb.behavior_buffer.extend(["test"] * 10)
+        self.behavior_detection.error_behavior = True
 
-        await behavior_detection.after_response(entry)
+        await self.behavior_detection.after_response(self.entry)
 
-        self.assertFalse(entry.arguments["error_behavior"])
-        self.assertFalse(behavior_detection.error_behavior)
+        self.assertFalse(self.entry.arguments["error_behavior"])
+        self.assertFalse(self.behavior_detection.error_behavior)
 
     @async_test()
     async def test_is_error_behavior_return_true_if_all_response_have_the_same_content(self):
-        behavior_detection = DetectBehaviorChange(buffer_size=10)
-        behavior_detection.response_simhash_buffer = [Simhash("data").value] * 10
-        response = StaticResponse(200, {}, content="data")
-        entry = Entry.create("http://example.com/", response=response)
+        self.behavior_detection.response_simhash_buffer = [Simhash("data").value] * 10
 
-        self.assertTrue(behavior_detection._is_error_behavior(entry))
+        error_behavior = self.behavior_detection._is_error_behavior(self.entry)
+
+        self.assertTrue(error_behavior)
 
     @async_test()
     async def test_is_error_behavior_return_false_if_not_all_response_have_the_same_content(self):
-        behavior_detection = DetectBehaviorChange(buffer_size=10)
-        behavior_detection.response_simhash_buffer = [Simhash("data").value] * 10
+        self.behavior_detection.response_simhash_buffer = [Simhash("data").value] * 10
         response = StaticResponse(200, {}, content="test")
         entry = Entry.create("http://example.com/", response=response)
 
-        self.assertFalse(behavior_detection._is_error_behavior(entry))
+        error_behavior = self.behavior_detection._is_error_behavior(entry)
+
+        self.assertFalse(error_behavior)
 
 
 class TestRejectBehaviorChange(TestCase):
 
+    def setUp(self):
+        self.heuristic = RejectBehaviorChange()
+        self.heuristic.behavior_change_detection = MagicMock()
+        response = StaticResponse(200, {}, content="test")
+        self.entry = Entry.create("http://example.com/", response=response)
+
     @async_test()
     async def test_after_response_check_for_behavior_change(self, loop):
-        heuristic = RejectBehaviorChange()
-        heuristic.behavior_change_detection = MagicMock()
-        heuristic.behavior_change_detection.after_response.return_value = fake_future(None, loop)
-        response = StaticResponse(200, {}, content="test")
-        entry = Entry.create("http://example.com/", response=response)
+        self.heuristic.behavior_change_detection.after_response.return_value = fake_future(None, loop)
 
-        await heuristic.after_response(entry)
+        await self.heuristic.after_response(self.entry)
 
-        heuristic.behavior_change_detection.after_response.assert_called_once_with(entry)
+        self.heuristic.behavior_change_detection.after_response.assert_called_once_with(self.entry)
 
     @async_test()
     async def test_after_response_raise_exception_if_behavior_changed(self, loop):
-        heuristic = RejectBehaviorChange()
-        heuristic.behavior_change_detection = MagicMock()
-        heuristic.behavior_change_detection.after_response.return_value = fake_future(None, loop)
+        self.heuristic.behavior_change_detection.after_response.return_value = fake_future(None, loop)
         response = StaticResponse(200, {}, content="test")
         entry = Entry.create("http://example.com/", response=response, arguments={"error_behavior": True})
 
         with self.assertRaises(BehaviorChanged):
-            await heuristic.after_response(entry)
+            await self.heuristic.after_response(entry)
