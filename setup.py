e8cb9e08ab7367bf3d4f7a04a0120e2a34e7bcb4
Nicolas <nicolas.aubry@delvelabs.ca>
2018-04-20 15:26:46 -0400

    Update requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
 from setuptools import setup
 
 
@@ -12,8 +8,6 @@ version_data = {}
 with open(version_file) as f:
     code = compile(f.read(), version_file, 'exec')
     exec(code, globals(), version_data)
-
-reqs = [str(x.req) for x in parse_requirements('./requirements.txt', session=False)]
 
 setup(name='HammerTime-http',
       version=version_data['__version__'],
@@ -24,6 +18,11 @@ setup(name='HammerTime-http',
       packages=['hammertime',
                 'hammertime.engine',
                 'hammertime.rules'],
-      install_requires=reqs,
+      install_requires=[
+          'aiohttp>=3.0.0,<3.2.0',
+          'easyinject==0.3',
+          'aiodns==1.1.1',
+          'simhash==1.8.0'
+      ],
       license="GPLv2"
      )
